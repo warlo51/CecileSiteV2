0de5e5d3d89bea08cd64d6576d6a5f499751a318
maxime mansuy <mmansuy@norauto.com>
2022-11-15 00:51:37 +0100

    deployment

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -41,9 +41,9 @@ export default async function handler(
     if (mailUserAuth0 === "cecile.fabie@gmail.com") {
       res.redirect(303, "/Admin");
     } else {
-      res.redirect(`/?email=${JSON.stringify(auth0searchUser)}`);
+      res.redirect(`/`);
     }
   } else {
-    res.redirect(`/?error=${JSON.stringify(auth0searchUser)}`);
+    res.redirect(`/`);
   }
 }
